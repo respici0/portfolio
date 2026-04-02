7cac33c87b9c141eb49da0064b2409b993f077b7
Robert Respicio <respicio.dev+github@gmail.com>
2026-04-01 21:55:58 -0700

    updated portfolio to be in progress

--- src/data/selectedWork/selectedWork.ts
+++ src/data/selectedWork/selectedWork.ts
@@ -34,14 +34,6 @@ export const companies: Company[] = [
           {
             type: "case study",
           },
-          // {
-          //   type: "live",
-          //   url: "some.url",
-          // },
-          // {
-          //   type: "github",
-          //   url: "github.url",
-          // },
         ],
         tech: [
           "sveltekit",
@@ -137,7 +129,7 @@ export const companies: Company[] = [
          "Although I have an eye for design, I'm not a UI/UX designer, so the collaboration with Claude on the design process (and some technical decisions) is worth reflecting on.",
         links: [
           {
-            type: "case study",
+            type: "case study in progress",
           },
           {
             type: "github",
